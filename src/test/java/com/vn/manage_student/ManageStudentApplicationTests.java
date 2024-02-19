fe9f1a4dd9253d656c7a9e0a646e59d4165a06f5
LeTuan <leetuaans03@gmail.com>
2024-02-19 12:34:49 +0700

    init project

---
+++ src/test/java/com/vn/manage_student/ManageStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.manage_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
